274064cb8cabb7339a7822fa705d447e19ad520c
felixhir <felix.hirschel@outlook.de>
2022-02-13 09:31:37 +0100

    tested electron a little

--- frontend/main.js
+++ frontend/main.js
@@ -1,9 +1,13 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, './src/preload.js')
+      }
     });
   
     win.loadFile('./src/index.html');
@@ -11,9 +15,4 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
     createWindow();
-});
-
-app.on("window-all-closed", () => {
-    console.log("The user has terminated the program!");
-    app.quit();
 });
