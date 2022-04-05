91db60a61d52c42e86652f9817b77e1519d0f1c5
felixhir <felix.hirschel@outlook.de>
2022-04-05 09:21:32 +0200

    minor dependency adjustments

--- backend/0-plugin-layer/src/main/java/dhbw/GainWatchersApplication.java
+++ backend/0-plugin-layer/src/main/java/dhbw/GainWatchersApplication.java
@@ -1,12 +1,21 @@
 package dhbw;
 
+import dhbw.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GainWatchersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GainWatchersApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner demo(CustomerRepository customerRepository) {
+        return (args -> {
+        });
     }
 }
