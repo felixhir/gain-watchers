f6869a2466fb5697abecd6c3e34b0116f5b25b63
felixhir <felix.hirschel@outlook.de>
2022-04-22 14:36:17 +0200

    More improvements to the frontend

--- frontend2/script.js
+++ frontend2/script.js
@@ -6,17 +6,32 @@ async function getResources(resource) {
     })
 }
 
-document.getElementById('button').addEventListener('click', () => {
-    getResources("customers").then(data => {
-        console.log(data)
-    })
-});
-
 document.addEventListener('DOMContentLoaded', () => {
     getResources("customers").then(customers => {
+        var customerList = document.createElement("ul")
+        for (var customer of customers) {
+            var item = document.createElement("li")
+            item.innerText = customer["name"]
+            customerList.appendChild(item)
+        }
+        document.getElementById("customer-div").appendChild(customerList);
     })
     getResources("exercises").then(exercises => {
+        var exerciseList = document.createElement("ul")
+        for (var exercise of exercises) {
+            var item = document.createElement("li")
+            item.innerText = exercise["name"]
+            exerciseList.appendChild(item)
+        }
+        document.getElementById("exercise-div").appendChild(exerciseList);
     })
     getResources("workouts").then(workouts => {
+        var workoutList = document.createElement("ul")
+        for (var workout of workouts) {
+            var item = document.createElement("li")
+            item.innerText = workout["name"]
+            workoutList.appendChild(item)
+        }
+        document.getElementById("workout-div").appendChild(workoutList);
     })
 })
