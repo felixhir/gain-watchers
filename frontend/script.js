587c1f3b9f498eacfbeb10e2f36602a9c72932fa
felixhir <felix.hirschel@outlook.de>
2022-04-22 23:38:21 +0200

    more frontend stuff

--- frontend/script.js
+++ frontend/script.js
@@ -6,32 +6,58 @@ async function getResources(resource) {
     })
 }
 
+function intoNameList(items) {
+    var list = document.createElement("ul")
+    for (var item of items) {
+        var listElement = document.createElement("li")
+        listElement.innerText = item["name"]
+        list.appendChild(listElement)
+    }
+    return list
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    reloadEntities()
+})
+
+function reloadEntities() {
     getResources("customers").then(customers => {
-        var customerList = document.createElement("ul")
-        for (var customer of customers) {
-            var item = document.createElement("li")
-            item.innerText = customer["name"]
-            customerList.appendChild(item)
-        }
-        document.getElementById("customer-div").appendChild(customerList);
+        var customerList = intoNameList(customers)
+        customerList.id = "customer-ul"
+        document.getElementById("customer-ul").replaceWith(customerList)
     })
     getResources("exercises").then(exercises => {
-        var exerciseList = document.createElement("ul")
-        for (var exercise of exercises) {
-            var item = document.createElement("li")
-            item.innerText = exercise["name"]
-            exerciseList.appendChild(item)
-        }
-        document.getElementById("exercise-div").appendChild(exerciseList);
+        var exerciseList = intoNameList(exercises)
+        exerciseList.id = "exercise-ul"
+        document.getElementById("exercise-ul").replaceWith(exerciseList)
     })
     getResources("workouts").then(workouts => {
-        var workoutList = document.createElement("ul")
-        for (var workout of workouts) {
-            var item = document.createElement("li")
-            item.innerText = workout["name"]
-            workoutList.appendChild(item)
-        }
-        document.getElementById("workout-div").appendChild(workoutList);
+        var workoutList = intoNameList(workouts)
+        workoutList.id = "workout-ul"
+        document.getElementById("workout-ul").replaceWith(workoutList)
     })
-})
+}
+
+function postCustomer() {
+    var form = document.getElementById("customer-form")
+    var formData = new FormData(form)
+    var jsonData =  Object.fromEntries(formData)
+    jsonData["height"] = parseInt(jsonData["height"])
+    jsonData["weight"] = parseInt(jsonData["weight"])
+    jsonData["bodyFatPercentage"] = parseInt(jsonData["bodyFatPercentage"])
+    jsonData["daysAvailablePerWeek"] = parseInt(jsonData["daysAvailablePerWeek"])
+
+    fetch(api_url + "customers", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(jsonData),
+    }).then(response => {
+        if (response.status == 201) {
+            reloadEntities()
+        } else {
+            window.alert("You have entered an invalid value!")
+        }
+    })
+}
